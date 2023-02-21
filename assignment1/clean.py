56f623ecaa75984ce781042cd4b286da901b07f7
cyrusting22465197 <22465197@life.hkbu.edu.hk>
2023-02-21 20:28:16 +0800

    data cleaning script

---
+++ assignment1/clean.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df1= pd.read_csv ('respondent_contact.csv')
+df2= pd.read_csv ('respondent_other.csv')
+
+#merge
+merge_file = pd.merge(df1, df2,
+                      left_on='respondent_id', right_on='id',
+                      how='outer')
+
+#drop
+df3=merge_file. drop(['id'],axis=1)
+df4=merge_file.dropna(axis=0)
+output_file=df4[df4['job'].str.contains('insurance|Insurance')==False]
+
+#export
+output_file.to_csv('output_file.csv',index=False)
